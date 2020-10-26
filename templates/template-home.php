1864cba9500c4f166509be6d13fd2bdfb9a664dd
Hansanghyeon <dandan9509@gmail.com>
2020-10-26 18:46:51 +0900

    feat: header 기본타입

--- templates/template-home.php
+++ templates/template-home.php
@@ -7,8 +7,13 @@
 
 get_header();
 
-$theme_url = get_stylesheet_directory_uri();
+$theme_url = get_stylesheet_directory();
 $img_url = get_home_url() . '/wp-content/uploads';
 
+include($theme_url . '/template-parts/header/default.php');
+?>
 
+
+
+<?php
 get_footer();
