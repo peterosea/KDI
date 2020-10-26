d66648459fd3e2eae0f79d7997b55d266bd787bf
Hansanghyeon <dandan9509@gmail.com>
2020-10-26 18:12:49 +0900

    feat: add bootstrap

--- functions.php
+++ functions.php
@@ -8,3 +8,12 @@
 foreach (glob(get_stylesheet_directory() . "/functions/*.php") as $filename) {
   include $filename;
 }
+
+function themeslug_enqueue()
+{
+  // Bootstrap
+  wp_enqueue_style('Bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', array(), null, false);
+  wp_enqueue_script('jQuery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), null, false);
+  wp_enqueue_script('Bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array(), null, false);
+}
+add_action('wp_enqueue_scripts', 'themeslug_enqueue');
