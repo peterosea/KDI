e0b48372743856f770bcf849105e5c34c2aab481
Hansanghyeon <dandan9509@gmail.com>
2020-10-26 18:18:14 +0900

    feat: add homePage template

---
+++ templates/template-home.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Template Name: Home
+ * Template Post Type: post, page
+ */
+
+get_header();
+
+$theme_url = get_stylesheet_directory_uri();
+$img_url = get_home_url() . '/wp-content/uploads';
+
+
+get_footer();
