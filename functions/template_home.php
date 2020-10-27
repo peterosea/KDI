77a687f9154daf44d57d46b5a64ba2f9c8a0d676
Hansanghyeon <dandan9509@gmail.com>
2020-10-27 12:12:49 +0900

    feat: footer

--- functions/template_home.php
+++ functions/template_home.php
@@ -10,5 +10,12 @@
 function font_8fd30c6f_6025_4027_9a2f_dff96a4d0826()
 {
   wp_enqueue_style('fonts google: fira code', 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap');
+  wp_enqueue_style('fonts google: fira code', 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap');
 }
 add_action('wp_enqueue_scripts', 'font_8fd30c6f_6025_4027_9a2f_dff96a4d0826');
+
+function footer_8fd30c6f_6025_4027_9a2f_dff96a4d0826()
+{
+  include get_stylesheet_directory() . '/template-parts/footer.php';
+}
+add_action('wp_footer', footer_8fd30c6f_6025_4027_9a2f_dff96a4d0826);
---
+++ template-parts/footer.php
@@ -0,0 +1,34 @@
+<?php
+$theme_url = get_stylesheet_directory();
+$img_url = get_home_url() . '/wp-content/uploads';
+?>
+<footer class="default">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <ul class="snsList">
+          <li>
+            <a href="#" class="box">
+              <img src="<?php echo $img_url ?>/1109.svg" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="#" class="box">
+              <img src="<?php echo $img_url ?>/1107.svg" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="<?php echo $img_url ?>/iconfinder-instagram-glyph-svg-5335781.svg" alt="">
+            </a>
+          </li>
+        </ul>
+        <div class='cc'>
+          <p>
+            Hanwha Copyright©2013 HANWHA GROUP ALL RIGHTS RESERVED.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
