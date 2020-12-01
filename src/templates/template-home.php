37a02b91437d0a0603832e5f53d6008698cdc2bb
Hansanghyeon <dandan9509@gmail.com>
2020-12-01 15:25:24 +0900

    feat: Section1 제플린 디자인 반영

--- src/templates/template-home.php
+++ src/templates/template-home.php
@@ -19,50 +19,7 @@ get_header();
 include($theme_url . '/template-parts/header/temp.php');
 ?>
 <main class="homePage">
-  <section class="section1">
-    <div class="slider">
-      <div class="slickSlideBg">
-        <div>
-          <img src="<?php echo $img_url ?>/10.jpg" alt="">
-        </div>
-      </div>
-      <div class="slickSlideText">
-        <div class="slide">
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <h1 class="title">안정적인 고객 만족 실현으로<br />
-                  신뢰받는 방산 체계업체</h1>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <span class="d-md-block dimText">고객에게 신뢰받는 방산업체로서 자주국방과</span>
-                <span class="d-md-block dimText">세계평화의 기틀이 되도록 하겠습니다.</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <script>
-        $('.slickSlideBg').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          fade: true,
-          asNavFor: '.slickSlideText'
-        });
-        $('.slickSlideText').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          dots: false,
-          dotsClass: 'slick-dots container',
-          asNavFor: '.slickSlideBg'
-        });
-      </script>
-    </div>
-  </section>
+  <?php include('template-home/section1.php') ?>
   <?php include('template-home/section2.php') ?>
   <section class="section3">
     <div class="container">
