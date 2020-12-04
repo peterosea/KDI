fb2b8bdd622bb4dab4560244511cdad9e2a4f9e3
Hansanghyeon <dandan9509@gmail.com>
2020-12-04 23:08:31 +0900

    :sparkles: 메인조직도 페이지로 이전

--- src/templates/template-home.php
+++ src/templates/template-home.php
@@ -16,27 +16,6 @@ get_header();
 <main class="homePage">
   <?php include('template-home/section1.php') ?>
   <?php include('template-home/section2.php') ?>
-  <section class="section3">
-    <div class="container">
-      <div class="row mb-5">
-        <div class="col">
-          <h1 class="title">조직도</h1>
-          <p>
-            <span class="d-md-block">KDI는 차세대 유도무기와 탄약 지능화를 연구하는 연구소와</span>
-            <span class="d-md-block">각 사업장 별로 특성화 품목 생산과 연구개발을 진행하고 있습니다.</span>
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col d-none d-lg-block">
-          <img src="<?php echo $img_url ?>/1565.png" alt="">
-        </div>
-        <div class="col d-lg-none">
-          <img src="<?php echo $img_url ?>/1568.png" alt="">
-        </div>
-      </div>
-    </div>
-  </section>
 </main>
 <?php
 get_footer();
