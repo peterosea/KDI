0b030509814c52dcd06c0de552a8938e68cc89ff
Hansanghyeon <999@hyeon.pro>
2021-01-11 15:18:56 +0900

    :sparkles: home - section1 acf로 필드 설정

--- kdi_ko/Theme/templates/template-home/section1.php
+++ kdi_ko/Theme/templates/template-home/section1.php
@@ -1,3 +1,6 @@
+<?php
+$section1 = get_field('section1');
+?>
 <section class="section1">
   <div class="slider">
     <div class="slickSlideBg">
@@ -13,20 +16,18 @@
           <div class="row">
             <div class="col">
               <h1 class="title">
-                안정적인 고객 만족 실현으로<br />
-                신뢰받는 방산 체계업체
+                <?php echo $section1['title'] ?>
               </h1>
             </div>
           </div>
           <div class="row content">
-            <div class="col">
-              <span class="d-md-block dimText">고객에게 신뢰받는 방산업체로서 자주국방과</span>
-              <span class="d-md-block dimText">세계평화의 기틀이 되도록 하겠습니다.</span>
+            <div class="col dimText">
+              <?php echo $section1['content'] ?>
             </div>
           </div>
           <div class="row">
             <div class="col">
-              <a href="/ceo-인사말/" class="btn"><span>회사소개 바로가기</span></a>
+              <a href="<?php echo $section1['button']['url'] ?>" class="btn"><span><?php echo $section1['button']['title'] ?></span></a>
             </div>
           </div>
         </div>
