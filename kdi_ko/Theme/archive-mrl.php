0897cf36209c2329843b5570f936b004329c870e
Hansanghyeon <999@hyeon.pro>
2021-01-13 20:23:19 +0900

    :sparkles: Awesome!! 아카이브페이지 전체 사용할 수 있는 헤더로 변경

--- kdi_ko/Theme/archive-mrl.php
+++ kdi_ko/Theme/archive-mrl.php
@@ -3,7 +3,7 @@ $theme_url = get_stylesheet_directory();
 $functions_url = get_stylesheet_directory() . '/functions';
 $img_url = get_home_url() . '/wp-content/uploads';
 get_header();
-get_template_part('template-parts/header/page', 'header-mrl');
+get_template_part('template-parts/header/page', 'header-archive');
 ?>
 <main class="templatePage mrlArchive">
   <div class="container">
